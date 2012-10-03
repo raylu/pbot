ef719039147d9e2194d2b6b5dddfccec2cc81b05
raylu <raylu@mixpanel.com>
2012-10-03 02:23:07 -0700

    fix major bugs with Connection.recv()
    self.last_buf was... very wrong

--- connection.py
+++ connection.py
@@ -1,15 +1,17 @@
 import socket
 
-socket.setdefaulttimeout(30)
+socket.setdefaulttimeout(180)
 
 class Connection:
 	def __init__(self):
 		self.socket = None
+		self.last_buf = None
+		self.debug = False
 
 	def send(self, *data):
 		line = ' '.join(data) + '\r\n'
+		if self.debug: print('->', line, end='')
 		self.socket.send(bytes(line, 'utf-8'))
-		self.last_buf = None
 
 	def recv(self):
 		data = str(self.socket.recv(4096), 'utf-8')
@@ -17,17 +19,13 @@ class Connection:
 			data = self.last_buf + data
 			self.last_buf = None
 		lines = data.split('\r\n')
-		if not len(lines):
-			self.last_buf = data
-			return
-
 		for i in range(len(lines) - 1):
+			if self.debug: print('<-', lines[i])
 			yield lines[i]
 		last = lines[-1]
-		if last[-2:] != '\r\n':
+		if last:
 			self.last_buf = last
 			return
-		yield last
 
 	def connect(self, host, port, nick, user):
 		self.socket = socket.create_connection((host, port))
